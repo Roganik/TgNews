de479e14bcfe5c1f897f120dd604308294f02583
Nikolai Rogalskii <roganik@gmail.com>
2022-02-16 15:56:11 +0700

    add new events

--- TgNews.BL/Client/TelegramEvents.cs
+++ TgNews.BL/Client/TelegramEvents.cs
@@ -45,6 +45,21 @@ public class TelegramEvents
                 case UpdateMessagePoll update:
                     OnUpdateMessagePoll?.Invoke(update); 
                     break;
+                case UpdateNewChannelMessage {message: Message msg} update:
+                    OnUpdateNewChannelMessage?.Invoke(update, msg);
+                    break;
+                case UpdateUserTyping update:
+                    OnUpdateUserTyping?.Invoke(update);
+                    break;
+                case UpdateChannelUserTyping update:
+                    OnUpdateChannelUserTyping?.Invoke(update);
+                    break;
+                case UpdateChannelMessageViews update:
+                    OnUpdateChannelMessageViews?.Invoke(update);
+                    break;
+                case UpdateNewMessage {message: Message msg} update:
+                    OnUpdateNewMessage?.Invoke(update, msg);
+                    break;
                 default:
                     OnUpdate?.Invoke(abstractUpdate);
                     break;
@@ -60,6 +75,11 @@ public class TelegramEvents
     public event Action<UpdateEditChannelMessage, MessageService>? OnUpdateEditChannelMessageService;
     public event Action<UpdateMessagePoll>? OnUpdateMessagePoll;
     public event Action<UpdateUserStatus>? OnUpdateUserStatus;
+    public event Action<UpdateNewChannelMessage, Message>? OnUpdateNewChannelMessage;
+    public event Action<UpdateUserTyping> OnUpdateUserTyping;
+    public event Action<UpdateChannelUserTyping> OnUpdateChannelUserTyping;
+    public event Action<UpdateChannelMessageViews> OnUpdateChannelMessageViews;
+    public event Action<UpdateNewMessage, Message> OnUpdateNewMessage;
 
     /// <summary> Handler for untyped messages </summary>
     public event Action<Update>? OnUpdate;
